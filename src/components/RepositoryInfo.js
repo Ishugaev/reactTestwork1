9a61845a58d8e329e26b00c8fa953a55038e69be
i.shugaev <i.shugaiev@levi9.com>
2017-09-19 22:38:11 +0300

    rewritten RepositoryInfo and RepositoryList on Functional Component #homework2

--- src/components/RepositoryInfo.js
+++ src/components/RepositoryInfo.js
@@ -1,37 +1,36 @@
-//full_name, html_url, description, language, owner.login, owner.avatar_url
-import React, { PureComponent } from 'react';
+import React from 'react';
 
-class RepositoryInfo extends PureComponent {
-    render() {
-        return (
-            <div className="col">
-                <div className="label">
-                    <div>Owner avatar: </div>
-                    <div><img src={this.props.repository.owner.avatar_url}/></div>
-                </div>
-                <div className="label">
-                    <span>Owner login: </span>
-                    <span>{this.props.repository.owner.login}</span>
-                </div>
-                <div className="label">
-                    <span>Full Name: </span>
-                    <span>{this.props.repository.full_name}</span>
-                </div>
-                <div className="label">
-                    <span>Description: </span>
-                    <span>{this.props.repository.description}</span>
-                </div>
-                <div className="label">
-                    <span>Language: </span>
-                    <span>{this.props.repository.language}</span>
-                </div>
-                <div className="label">
-                    <span>Html url: </span>
-                    <span>{this.props.repository.html_url}</span>
-                </div>
+const RepositoryInfo = (prop) => {
+    return prop.repository.owner
+        ?
+        <div className="col">
+            <div className="label">
+                <div>Owner avatar: </div>
+                <div><img src={prop.repository.owner.avatar_url}/></div>
             </div>
-        );
-    }
+            <div className="label">
+                <span>Owner login: </span>
+                <span>{prop.repository.owner.login}</span>
+            </div>
+            <div className="label">
+                <span>Full Name: </span>
+                <span>{prop.repository.full_name}</span>
+            </div>
+            <div className="label">
+                <span>Description: </span>
+                <span>{prop.repository.description}</span>
+            </div>
+            <div className="label">
+                <span>Language: </span>
+                <span>{prop.repository.language}</span>
+            </div>
+            <div className="label">
+                <span>Html url: </span>
+                <span>{prop.repository.html_url}</span>
+            </div>
+        </div>
+        :
+        <span>Loading...</span>;
 }
 
 export default RepositoryInfo
--- src/components/RepositoryList.js
+++ src/components/RepositoryList.js
@@ -1,21 +1,19 @@
-import React,    { PureComponent } from 'react';
+import React from 'react';
 import Repository from './Repository';
 
-class RepositoryList extends PureComponent {
-    render() {
-        return (
-            <nav className="col">
-                {
-                    this.props.repositoryList.map(repository => (
-                        <div className="repository">
-                            <span>{repository === this.props.selected  ? '>' : ''}</span>
-                            <Repository key={repository.name} repository={repository} onClick={this.props.onClick} />
-                        </div>
-                    ))
-                }
+const RepositoryList = (prop) => {
+    return (
+        <nav className="col">
+            {
+                prop.repositoryList.map(repository => (
+                    <div className="repository">
+                        <span>{repository === prop.selected  ? '>' : ''}</span>
+                        <Repository key={repository.name} repository={repository} onClick={prop.onClick} />
+                    </div>
+                ))
+            }
             </nav>
-        );
-    }
+    );
 }
 
 export default RepositoryList;
